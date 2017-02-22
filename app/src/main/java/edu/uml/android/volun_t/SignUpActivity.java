36b115536db169daccdbc107b303b3fdbebce820
Adam Gaudreau <adam@Adams-MacBook-Air.local>
2017-02-22 10:11:40 -0500

    Completed Sign Up activity

--- app/src/main/java/edu/uml/android/volun_t/SignUpActivity.java
+++ app/src/main/java/edu/uml/android/volun_t/SignUpActivity.java
@@ -7,64 +7,74 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
-    private EditText inputEmail, inputPassword;
+    private DatabaseReference db;
+    private EditText inputEmail, inputPassword, inputPassword2;
+    private EditText inputFirstV, inputLastV, inputAddressV, inputPhoneV, inputMake, inputModel, inputLicense, inputSeats;
+    private EditText inputFirstC, inputLastC, inputAddressC, inputPhoneC;
+    private CheckBox checkHandicap;
     private Button buttonSignUp, buttonCancel;
-    private TextView textAlreadyHaveAcct;
+    private RadioButton radioOffer, radioNeed;
     private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_startup);
+        setContentView(R.layout.activity_signup);
 
         // Get the firebase authentication instance
         auth = FirebaseAuth.getInstance();
-        /* Find the views
+        db = FirebaseDatabase.getInstance().getReference();
+        // Find the views
         inputEmail = (EditText) findViewById(R.id.email_field);
         inputPassword = (EditText) findViewById(R.id.password_field);
+        inputPassword2 = (EditText) findViewById(R.id.password_field_2);
+        inputFirstV = (EditText) findViewById(R.id.first_name_volunteer_field);
+        inputLastV = (EditText) findViewById(R.id.last_name_volunteer_field);
+        inputAddressV = (EditText) findViewById(R.id.address_volunteer_field);
+        inputPhoneV = (EditText) findViewById(R.id.phone_volunteer_field);
+        inputMake = (EditText) findViewById(R.id.make_field);
+        inputModel = (EditText) findViewById(R.id.model_field);
+        inputLicense = (EditText) findViewById(R.id.license_plate_field);
+        inputSeats = (EditText) findViewById(R.id.seats_field);
+        inputFirstC = (EditText) findViewById(R.id.first_name_client_field);
+        inputLastC = (EditText) findViewById(R.id.last_name_client_field);
+        inputAddressC = (EditText) findViewById(R.id.address_client_field);
+        inputPhoneC = (EditText) findViewById(R.id.phone_client_field);
+        checkHandicap = (CheckBox) findViewById(R.id.handicap_field);
         buttonSignUp = (Button) findViewById(R.id.signup_button);
         buttonCancel = (Button) findViewById(R.id.cancel_button);
-        textAlreadyHaveAcct = (TextView) findViewById(R.id.already_text);
+        radioNeed = (RadioButton) findViewById(R.id.radio_need_help);
+        radioOffer = (RadioButton) findViewById(R.id.radio_offer_help);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        */
+
         // Set up on click listeners
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Make sure the email and password fields are filled in
-                String email = inputEmail.getText().toString().trim();
-                String password = inputPassword.getText().toString().trim();
-                // If email is empty
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter an email address!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                // If password is empty
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter a password! (Must be 6 or more characters long.)", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                // If password is too short
-                if (password.length() < 6) {
-                    Toast.makeText(getApplicationContext(), "Password must be 6 or more characters long.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                // Make sure all fields have been entered
+                final User user = createUser();
+                if(!validateFields(user)) return;
                 // We should be good to go, no create the user
                 progressBar.setVisibility(View.VISIBLE);
-                auth.createUserWithEmailAndPassword(email, password)
+                auth.createUserWithEmailAndPassword(user.getEmail(), user.getPass())
                         .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -76,8 +86,9 @@ public class SignUpActivity extends AppCompatActivity {
                                     Toast.makeText(SignUpActivity.this, "Error in creating account: " + task.getException(),
                                             Toast.LENGTH_SHORT).show();
                                 } else {
-                                    Toast.makeText(SignUpActivity.this, "Account created!" + task.getException(),
+                                    Toast.makeText(SignUpActivity.this, "Account created!",
                                             Toast.LENGTH_SHORT).show();
+                                    addUserToDatabase(user);
                                     startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
                                     finish();
                                 }
@@ -86,19 +97,130 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+        radioNeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout needView = (LinearLayout) findViewById(R.id.layout_need_help);
+                LinearLayout offerView = (LinearLayout) findViewById(R.id.layout_offer_help);
+                LinearLayout vehicleView = (LinearLayout) findViewById(R.id.layout_vehicle_info);
+                needView.setVisibility(View.VISIBLE);
+                offerView.setVisibility(View.GONE);
+                vehicleView.setVisibility(View.GONE);
+                buttonSignUp.setVisibility(View.VISIBLE);
+                buttonCancel.setVisibility(View.VISIBLE);
+            }
+        });
+
+        radioOffer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout needView = (LinearLayout) findViewById(R.id.layout_need_help);
+                LinearLayout offerView = (LinearLayout) findViewById(R.id.layout_offer_help);
+                LinearLayout vehicleView = (LinearLayout) findViewById(R.id.layout_vehicle_info);
+                needView.setVisibility(View.GONE);
+                offerView.setVisibility(View.VISIBLE);
+                vehicleView.setVisibility(View.VISIBLE);
+                buttonSignUp.setVisibility(View.VISIBLE);
+                buttonCancel.setVisibility(View.VISIBLE);
+            }
+        });
+
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(SignUpActivity.this, StartupActivity.class));
             }
         });
+    }
 
-        textAlreadyHaveAcct.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    protected User createUser() {
+        String email = inputEmail.getText().toString().trim();
+        String pass1 = inputPassword.getText().toString().trim();
+        String pass2 = inputPassword2.getText().toString().trim();
 
-            }
-        });
+        String first = "", last = "", address = "", phone = "", make = "", model = "", plate = "";
+        int seats = 0, type = -1;
+        boolean handicap = false;
+
+        if (radioOffer.isChecked()) {
+            type = 1;
+            first = inputFirstV.getText().toString().trim();
+            last = inputLastV.getText().toString().trim();
+            address = inputAddressV.getText().toString().trim();
+            phone = inputPhoneV.getText().toString().trim();
+            make = inputMake.getText().toString().trim();
+            model = inputModel.getText().toString().trim();
+            plate = inputLicense.getText().toString().trim();
+            if (!TextUtils.isEmpty(inputSeats.getText().toString().trim()))
+                seats = Integer.parseInt(inputSeats.getText().toString().trim());
+            handicap = checkHandicap.isChecked();
+        } else if (radioNeed.isChecked()) {
+            type = 0;
+            first = inputFirstC.getText().toString().trim();
+            last = inputLastC.getText().toString().trim();
+            address = inputAddressC.getText().toString().trim();
+            phone = inputPhoneC.getText().toString().trim();
+        }
+
+        return new User(email, pass1, pass2, first, last, address, phone, make, model, plate, seats, type, handicap);
 
     }
+
+    // Returns TRUE if all values in fields are enough to sign up for an account
+    protected boolean validateFields(User user) {
+        // If passwords do not match
+        if (!user.getPass().equals(user.getPass2())) {
+            Toast.makeText(getApplicationContext(), "Passwords do not match.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        // If password is too short
+        if (user.getPass().length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password must be 6 or more characters long.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        // See if other fields were missing
+        if (TextUtils.isEmpty(user.getEmail()) || TextUtils.isEmpty(user.getFirst()) || TextUtils.isEmpty(user.getLast()) ||
+                TextUtils.isEmpty(user.getAddress()) || TextUtils.isEmpty(user.getPhone())) {
+            Toast.makeText(getApplicationContext(), "Missing personal information, please check.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (user.getType() == 1) {
+            if (TextUtils.isEmpty(user.getMake()) || TextUtils.isEmpty(user.getModel()) || TextUtils.isEmpty(user.getPlate()) ||
+                user.getSeats() <= 0) {
+                Toast.makeText(getApplicationContext(), "Missing vehicle information, please check.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected boolean addUserToDatabase(User user) {
+        // Get user ID
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+        String uid = firebaseUser.getUid();
+
+        db.child("users").child(uid).child("email").setValue(user.getEmail());
+        db.child("users").child(uid).child("first_name").setValue(user.getFirst());
+        db.child("users").child(uid).child("last_name").setValue(user.getLast());
+        db.child("users").child(uid).child("address").setValue(user.getAddress());
+        db.child("users").child(uid).child("phone_number").setValue(user.getPhone());
+
+        if (user.getType() == 0) {
+            db.child("users").child(uid).child("type").setValue("client");
+        } else if (user.getType() == 1) {
+            db.child("users").child(uid).child("type").setValue("volunteer");
+            db.child("users").child(uid).child("make").setValue(user.getMake());
+            db.child("users").child(uid).child("model").setValue(user.getModel());
+            db.child("users").child(uid).child("license_plate").setValue(user.getPlate());
+            db.child("users").child(uid).child("number_of_seats").setValue(user.getSeats());
+            db.child("users").child(uid).child("handicap_access").setValue(user.getHandicap());
+        } else {
+            db.child("users").child(uid).child("type").setValue("!UNKNOWN!");
+            return false;
+        }
+
+        return true;
+    }
+
 }
---
+++ app/src/main/java/edu/uml/android/volun_t/User.java
@@ -0,0 +1,45 @@
+package edu.uml.android.volun_t;
+
+/**
+ * Created by adam on 2/16/17.
+ */
+
+public class User {
+
+    private String email, pass, pass2, first, last, address, phone, make, model, plate;
+    private int seats, type;  // type = 0 when client, type = 1 when volunteer
+    private boolean handicap;
+
+    public User(String email, String pass, String pass2, String first, String last, String address,
+                String phone, String make, String model, String plate, int seats, int type,
+                boolean handicap) {
+        this.email = email;
+        this.pass = pass;
+        this.pass2 = pass2;
+        this.first = first;
+        this.last = last;
+        this.address = address;
+        this.phone = phone;
+        this.make = make;
+        this.model = model;
+        this.plate = plate;
+        this.seats = seats;
+        this.type = type;
+        this.handicap = handicap;
+    }
+
+    public String getEmail() { return email; }
+    public String getPass() { return pass; }
+    public String getPass2() { return pass2; }
+    public String getFirst() { return first; }
+    public String getLast() { return last; }
+    public String getAddress() { return address; }
+    public String getPhone() { return phone; }
+    public String getMake() { return make; }
+    public String getModel() { return model; }
+    public String getPlate() { return plate; }
+    public int getSeats() { return seats; }
+    public int getType() { return type; }
+    public boolean getHandicap() {return handicap; }
+
+}
