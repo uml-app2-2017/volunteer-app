765b8d3f1237c026d25b76efeb138cc1a2d47d97
timLarocque <timothy_larocque@student.uml.edu>
2017-02-22 22:38:51 -0500

    XML for sign-in activity

--- app/src/main/java/edu/uml/android/volun_t/SignInActivity.java
+++ app/src/main/java/edu/uml/android/volun_t/SignInActivity.java
@@ -12,6 +12,6 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_startup);
+        setContentView(R.layout.activity_signin);
     }
 }
